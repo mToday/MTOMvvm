1a5cbc6a8e94169003bb20fef1e3f2f1c384336c
chenhuiming <3219066618@qq.com>
2019-11-21 09:19:14 +0800

    Activity管理

--- lib_common/src/main/java/nik/nile/common/base/BaseApplication.java
+++ lib_common/src/main/java/nik/nile/common/base/BaseApplication.java
@@ -1,14 +1,54 @@
 package nik.nile.common.base;
 
+import android.app.Activity;
 import android.app.Application;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 /**
  * @author : ChenHuiMing
  * @date : 2019-11-20
  * @email : 907399255@qq.com
  * description : Application 基类
- * 主要处理Application常见的分包等处理
+ * 主要处理Application常见的分包等处理，ActivityLifecycleCallbacks Activity全局监控
  */
-public class BaseApplication extends Application {
+public class BaseApplication extends Application implements Application.ActivityLifecycleCallbacks {
 
+
+    @Override
+    public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
+        AppManager.getAppManager().addActivity(activity);
+    }
+
+    @Override
+    public void onActivityStarted(@NonNull Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityResumed(@NonNull Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityPaused(@NonNull Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityStopped(@NonNull Activity activity) {
+
+    }
+
+    @Override
+    public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle outState) {
+
+    }
+
+    @Override
+    public void onActivityDestroyed(@NonNull Activity activity) {
+        AppManager.getAppManager().removeActivity(activity);
+    }
 }
